689a70f91841313c38e3a638b0bd41b552644a7a
ShohruhUzDev <shohruh7464784@mail.ru>
2023-02-08 15:19:14 +0500

    Add Foods folder and Food and Category classes

---
+++ BelissimoCloneWPF.Domain/Entities/Foods/Category.cs
@@ -0,0 +1,14 @@
+﻿using BelissimoCloneWPF.Domain.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BelissimoCloneWPF.Domain.Entities.Foods
+{
+    public class Category:Auditable
+    {
+        public string Content { get; set; }
+    }
+}
---
+++ BelissimoCloneWPF.Domain/Entities/Foods/Food.cs
@@ -0,0 +1,22 @@
+﻿using BelissimoCloneWPF.Domain.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BelissimoCloneWPF.Domain.Entities.Foods
+{
+    public class Food:Auditable
+    {
+        public string  Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public int AttachmentId { get; set; }
+
+
+    }
+}
