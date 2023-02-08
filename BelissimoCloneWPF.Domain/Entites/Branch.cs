a2cd81a93416d2b72dc6304f2926043eb9d593de
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-02-08 11:07:59 +0500

    Branch class created in Entites folder

---
+++ BelissimoCloneWPF.Domain/Entites/Branch.cs
@@ -0,0 +1,11 @@
+﻿using BelissimoCloneWPF.Domain.Commons;
+
+namespace BelissimoCloneWPF.Domain.Entites.Branches
+{
+    public sealed class Branch : Auditable
+    {
+        public string Operators { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+}
