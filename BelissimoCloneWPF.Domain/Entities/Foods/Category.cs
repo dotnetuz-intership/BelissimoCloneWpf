78c38ca766941867e2dfc8841a055b916da4cd9d
ShohruhUzDev <shohruh7464784@mail.ru>
2023-02-08 21:42:41 +0500

    ShohruhUzDev:I changed you said

--- BelissimoCloneWPF.Domain/Entities/Foods/Category.cs
+++ BelissimoCloneWPF.Domain/Entities/Foods/Category.cs
@@ -1,13 +1,8 @@
 ﻿using BelissimoCloneWPF.Domain.Commons;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BelissimoCloneWPF.Domain.Entities.Foods
 {
-    public sealed class Category:Auditable
+    public sealed class Category : Auditable
     {
         public string Content { get; set; }
     }
--- BelissimoCloneWPF.Domain/Entities/Foods/Food.cs
+++ BelissimoCloneWPF.Domain/Entities/Foods/Food.cs
@@ -1,14 +1,8 @@
 ﻿using BelissimoCloneWPF.Domain.Commons;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BelissimoCloneWPF.Domain.Entities.Foods
 {
-    public sealed class Food:Auditable
+    public sealed class Food : Auditable
     {
         public string  Name { get; set; }
         public string Description { get; set; }
