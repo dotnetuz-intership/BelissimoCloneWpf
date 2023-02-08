41811e748793a4b74ab27050a1629119a03a63fa
KholmatovKhabibulla <xabibullaxolmatov@gmail.com>
2023-02-08 20:49:59 +0500

    DATA: Created Busket Model

---
+++ BelissimoCloneWPF.Domain/Entities/Baskets/Basket.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BelissimoCloneWPF.Domain.Entities.Baskets
+{
+    internal class Basket
+    {
+    }
+}
