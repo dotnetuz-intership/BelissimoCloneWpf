8edd3a8b2ca238780bc923e66bc30cb64e001e46
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-02-13 00:33:21 +0500

    SERVICES: BranchForCreationDTO successfully created in DTOs/Branches folder

---
+++ BelissimoCloneWPF.Service/DTOs/Branches/BranchForCreationDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BelissimoCloneWPF.Service.DTOs.Branches
+{
+    public class BranchForCreationDTO
+    {
+
+        [Required]
+        public string Operators { get; set; }
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        public string Location { get; set; }
+
+    }
+}
