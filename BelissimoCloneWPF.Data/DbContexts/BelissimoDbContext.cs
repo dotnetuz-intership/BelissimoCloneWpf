e27ad7d2fd85b929527735e3bde78270aa7637da
VIPMan0001 <secretiveperson0001@gmail.com>
2023-02-13 01:00:36 +0500

    Data: Created DbContext Folder and class within it

---
+++ BelissimoCloneWPF.Data/DbContexts/BelissimoDbContext.cs
@@ -0,0 +1,30 @@
+﻿using BelissimoCloneWPF.Domain.Entities.Attachment;
+using BelissimoCloneWPF.Domain.Entities.Branches;
+using BelissimoCloneWPF.Domain.Entities.Foods;
+using BelissimoCloneWPF.Domain.Entities.Orders;
+using BelissimoCloneWPF.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace BelissimoCloneWPF.Data.DbContexts
+{
+    public class BelissimoDbContext : DbContext
+    {
+        public BelissimoDbContext(DbContextOptions<BelissimoDbContext> options) : base(options)
+        {
+
+        }
+        public virtual DbSet<Attachments> Attachments { get; set; }
+        public virtual DbSet<Branch> Branches { get; set; }
+        public virtual DbSet<Food> Foods { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<FoodOrder> FoodOrders { get; set; }
+        public virtual DbSet<Basket> Baskets { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost; Database=DbBelissimoCloneWPF; Username=postgres; Password=1234");
+        }
+    }
+    
+}
