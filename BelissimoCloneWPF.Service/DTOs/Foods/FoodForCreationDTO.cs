6bb180f2ad0b0ef8d1344cf96ed8ac1049120007
ShohruhUzDev <shohruh7464784@mail.ru>
2023-02-13 14:31:54 +0500

    Service:Changed AttachmentsId To AttachmentId

--- BelissimoCloneWPF.Service/DTOs/Foods/FoodForCreationDTO.cs
+++ BelissimoCloneWPF.Service/DTOs/Foods/FoodForCreationDTO.cs
@@ -6,6 +6,6 @@
         public string Description { get; set; }
         public int Price { get; set; }
         public int CategoryId { get; set; }
-        public int AttachmentsId { get; set; }
+        public int AttachmentId { get; set; }
     }
 }
