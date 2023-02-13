11dc54928a279f3e5a67e086a2616dbfe5919f5c
Alimkhan <olimxonnutfullaevich@gmail.com>
2023-02-13 20:45:58 +0500

    Service: CategoryForCreationgDTO created

---
+++ BelissimoCloneWPF.Service/DTOs/Category/CategoryForCreationDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BelissimoCloneWPF.Service.DTOs.Category
+{
+    public class CategoryForCreationDTO
+    {
+        [Required]
+        public string Context { get; set; }
+    }
+}
