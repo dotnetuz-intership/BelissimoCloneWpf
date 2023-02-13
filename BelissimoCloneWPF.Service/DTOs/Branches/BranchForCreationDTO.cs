009d19c8f44e1d0d676d371cfe9611cede0f86fc
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-02-13 22:58:23 +0500

    SERVICE: all unused lines deleted

--- BelissimoCloneWPF.Service/DTOs/Branches/BranchForCreationDTO.cs
+++ BelissimoCloneWPF.Service/DTOs/Branches/BranchForCreationDTO.cs
@@ -1,17 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
 namespace BelissimoCloneWPF.Service.DTOs.Branches
 {
     public class BranchForCreationDTO
     {
-
         [Required]
         public string Operators { get; set; }
         [Required]
         public string Name { get; set; }
-        
         [Required]
         public string Location { get; set; }
-
     }
 }
