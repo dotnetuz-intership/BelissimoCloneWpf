bf87619105ae9eda9a1f0929f93e2de4cd4d38cf
ShohruhUzDev <shohruh7464784@mail.ru>
2023-02-20 20:14:21 +0500

    SERVICE : Create EnvironmentHelper Class

---
+++ BelissimoCloneWPF.Service/Helpers/EnvironmentHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace BelissimoCloneWPF.Service.Helpers
+{
+    public static class EnvironmentHelper
+    {
+        public static string WebRootPath { get; set; }
+        public static string ExcelRootPath => Path.Combine(WebRootPath, "excels");
+        public static string FilePath => "images";
+    }
+}
