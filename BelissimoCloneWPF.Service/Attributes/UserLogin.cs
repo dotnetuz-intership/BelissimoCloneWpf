83522f3b58fb6e1b76ef5ed0684f8ff282e199fd
ShohruhUzDev <shohruh7464784@mail.ru>
2023-02-20 20:25:19 +0500

    SERVICE : Create UserLogin Attribute

---
+++ BelissimoCloneWPF.Service/Attributes/UserLogin.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BelissimoCloneWPF.Service.Attributes
+{
+    public class UserLogin : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+            => value is string login &&
+             login.All(c => char.IsDigit(c) ||
+             c == '_');       
+    }
+}
