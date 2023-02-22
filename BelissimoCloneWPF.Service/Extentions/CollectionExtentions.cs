e8d3bc2dbfe232cda1554400a94fe69dcc994c4f
Haydarovcoder <abboshaydarov0799@gmail.com>
2023-02-22 11:54:12 +0500

    SERVICE: Created FormFileAttributes Class in Attributes folder

--- BelissimoCloneWPF.Service/Extentions/CollectionExtentions.cs
+++ BelissimoCloneWPF.Service/Extentions/CollectionExtentions.cs
@@ -6,9 +6,12 @@ namespace BelissimoCloneWPF.Service.Extentions
     {
         public static IQueryable<T> ToPagedList<T>(this IQueryable<T> source, PaginationParams @params)
         {
+            var value = ((@params.PageIndex - 1) * @params.PageSize);
+
             return @params.PageIndex > 0 && @params.PageSize >= 0
-                ? source.Take(((@params.PageIndex - 1) * @params.PageSize)..@params.PageSize)
+                ? source.Take(value..(value + @params.PageSize))
                 : source;
         }
+  
     }
 }
