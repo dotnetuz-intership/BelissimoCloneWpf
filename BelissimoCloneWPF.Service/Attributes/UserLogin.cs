6ed4d884efc26bc7ae9a8122f502cb6a48c81ae7
ShohruhUzDev <shohruh7464784@mail.ru>
2023-02-22 19:23:17 +0500

    SERVICE : Create UserLogin Attribute

---
+++ BelissimoCloneWPF.Service/Attributes/UserLogin.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+    
+namespace BelissimoCloneWPF.Service.Attributes
+{
+    public class UserLogin : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (!(value is string Login &&
+                 Login.All(c => char.IsDigit(c) && c == '_')))
+                return new ValidationResult("username must be only digits and underline");
+
+            return ValidationResult.Success;
+        }
+    }
+}
