c33b1e963f67c7342453f3a7ef1433334c0bba13
Alimkhan <olimxonnutfullaevich@gmail.com>
2023-02-22 12:42:43 +0500

    CODE RUB: Unnecessary Code Line Removed

--- BelissimoCloneWPF.Service/Attributes/UserPassword.cs
+++ BelissimoCloneWPF.Service/Attributes/UserPassword.cs
@@ -13,29 +13,14 @@ namespace BelissimoCloneWPF.Service.Attributes
         protected override ValidationResult? IsValid(object value, ValidationContext validationContext)
         {
             var values = (string)value;
-            
+
             if (!(values is string password && password.Length >= 8 &&
                     password.Any(c => char.IsDigit(c)) &&
                         password.Any(c => char.IsLetter(c))))
-                return new ValidationResult("password must be at least one number and at least one letter and number of characters more or equal to 8");
-
+                return new ValidationResult("password must be at least one number and " +
+                    "at least one letter and number of characters more or equal to 8");
 
             return ValidationResult.Success;
         }
-        
-            
-            
-            
-        
-
-
-
-
-        /*if(value is string password && password.Length >= 8 && password.Any(c => char.IsDigit(c)) && password.Any(c => char.IsLetter(c))
-        {
-            return base.IsValid(value);
-        }*/
-
-
     }
 }
