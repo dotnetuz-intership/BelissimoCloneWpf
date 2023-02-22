c60e7d9daed54a942816a790dfbba3285169484b
Alimkhan <olimxonnutfullaevich@gmail.com>
2023-02-22 12:38:16 +0500

    CODE RUB: Override Return Type Of UserPassword Changed To ValidationResult

--- BelissimoCloneWPF.Service/Attributes/UserPassword.cs
+++ BelissimoCloneWPF.Service/Attributes/UserPassword.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,32 @@ namespace BelissimoCloneWPF.Service.Attributes
 {
     public class UserPassword : ValidationAttribute
     {
-        public override bool IsValid(object value)
-            => value is string password &&
-                password.Length >= 8 &&
+        protected override ValidationResult? IsValid(object value, ValidationContext validationContext)
+        {
+            var values = (string)value;
+            
+            if (!(values is string password && password.Length >= 8 &&
                     password.Any(c => char.IsDigit(c)) &&
-                        password.Any(c => char.IsLetter(c));
+                        password.Any(c => char.IsLetter(c))))
+                return new ValidationResult("password must be at least one number and at least one letter and number of characters more or equal to 8");
+
+
+            return ValidationResult.Success;
+        }
+        
+            
+            
+            
+        
+
+
+
+
+        /*if(value is string password && password.Length >= 8 && password.Any(c => char.IsDigit(c)) && password.Any(c => char.IsLetter(c))
+        {
+            return base.IsValid(value);
+        }*/
+
+
     }
 }
