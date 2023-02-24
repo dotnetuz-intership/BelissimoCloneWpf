ae3df4db772a81552dc67af7a613ad15668a0617
VIPMan0001 <secretiveperson0001@gmail.com>
2023-02-24 20:36:14 +0500

    SERVICE : Created UserForViewDTO and UserForUpdateDTO

---
+++ BelissimoCloneWPF.Service/DTOs/Users/UserForUpdateDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BelissimoCloneWPF.Service.DTOs.Users
+{
+    public class UserForUpdateDTO
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+    }
+}
