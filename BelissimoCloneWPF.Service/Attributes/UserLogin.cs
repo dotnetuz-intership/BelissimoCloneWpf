5a0f7338ba7837b3560448aee65ee36eb7ac1dc3
ShohruhUzDev <shohruh7464784@mail.ru>
2023-02-25 17:10:29 +0500

    CODE RUB : Fix Mistakes

--- BelissimoCloneWPF.Service/Attributes/UserLogin.cs
+++ BelissimoCloneWPF.Service/Attributes/UserLogin.cs
@@ -6,9 +6,9 @@ namespace BelissimoCloneWPF.Service.Attributes
     {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            if (!(value is string Login &&
-                 Login.All(c => char.IsDigit(c) && c == '_')))
-                return new ValidationResult("username must be only digits and underline");
+            if (!(value is string userLogin &&
+                 userLogin.All(c => char.IsDigit(c) && c == '_')))
+                return new ValidationResult("Username must be only digits and underline");
 
             return ValidationResult.Success;
         }
